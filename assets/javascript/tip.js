531c31e2b15a5cd5d3d029ac14eb8b17aeeda50c
VTC <vtchris@hotmail.com>
2019-06-09 22:17:37 -0400

    basic files/folders added

---
+++ assets/javascript/tip.js
@@ -0,0 +1,9 @@
+
+var subtotal = document.getElementById("subtotal-txt");
+var tax = document.getElementById("tax-txt");
+var total = document.getElementById("total-txt");
+var split = document.getElementById("split-txt");
+//document.getElementById("");
+
+subtotal.value = 0;
+split.value = 1;
